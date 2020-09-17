c29807634aede850c9868f2ebee8fbc895ef6533
Apdet <deterding1.a@gmail.com>
2020-09-17 17:08:44 -0500

    implemented firebasae into app

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvBOr780ut77HyXUpbfFwlM-dzVuAdOoE",
+    authDomain: "clone-1f2d7.firebaseapp.com",
+    databaseURL: "https://clone-1f2d7.firebaseio.com",
+    projectId: "clone-1f2d7",
+    storageBucket: "clone-1f2d7.appspot.com",
+    messagingSenderId: "1016073822787",
+    appId: "1:1016073822787:web:2bd90699aaa4e0d5fcaaf6",
+    measurementId: "G-XK1TLKFGKT"
+  };
