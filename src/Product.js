6f6fcd33fc664a323b15bd895d240e1084747b24
Apdet <deterding1.a@gmail.com>
2020-09-17 19:47:54 -0500

    subtotal items and total price is working

--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,34 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
-        <p className="product_price">
+        <p className="product__price">
           <small>$</small>
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
-        {Array(rating)
+          {Array(rating)
             .fill()
             .map((_, i) => (
               <p>🌟</p>
@@ -19,9 +36,9 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
 
-      <img src={image} />
+      <img src={image} alt="" />
 
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
